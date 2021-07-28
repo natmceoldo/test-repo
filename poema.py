a5a0ca0b4e87a6f96dbeb89294f97fcd2acb3c66
Natália Ceoldo <nat.mceoldo@gmail.com>
2021-07-27 22:21:00 -0300

    add Ney Mato Grosso do TI (shared by Mari)

---
+++ poema.py
@@ -0,0 +1,15 @@
+print('''Ney Matogrosso do TI:
+(ao som de O Vira)
+
+"O projeto atrasado cruzou a deadline
+Passou por debaixo da pipeline
+E lá no ambiente azul
+Na noite do deploy
+A internet iluminou
+O ticket que foi pra festa
+Jira Jira Jira,
+Jira Jira!
+Jira Home
+Jira! Jira!
+Jira Project Folder..."
+''')
